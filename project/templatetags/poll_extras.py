c1f91dee59610cb29a7a93119413ca1a4ba6aac6
Victor Kornienko <raidhorr@gmail.com>
2022-09-23 08:54:17 +0300

    Ошибка загрузки пользовательского тэга

--- project/templatetags/poll_extras.py
+++ project/templatetags/poll_extras.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django import template
 
 
@@ -10,17 +12,8 @@ CURRENCIES_SYMBOLS = {
 }
 
 
-@register.filter()
-def currency(value, code='rub'):
-    """
-    value: значение, к которому нужно применить фильтр
-    code: код валюты
-    """
-    postfix = CURRENCIES_SYMBOLS[code]
-
-    return f'{value} {postfix}'
-
-
 @register.simple_tag()
 def current_time(format_string='%b %d %Y'):
     return datetime.utcnow().strftime(format_string)
+
+
